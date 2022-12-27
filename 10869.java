12b74425dd87c8c4ce8cc551adbe53751b182e94
Dowon Kang (강도원) <91768420+onekorea37@users.noreply.github.com>
2022-12-26 23:43:36 -0500

    +,-,*,/,%

--- 10869.java
+++ 10869.java
@@ -24,15 +24,15 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+	BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(reader.readLine());
-        int A = Integer.parseInt(st.nextToken());
-        int B = Integer.parseInt(st.nextToken());
-        System.out.println(A + B);
-        System.out.println(A - B);
-        System.out.println(A * B);
-        System.out.println(A / B);
-        System.out.println(A % B);
+          int A = Integer.parseInt(st.nextToken());
+      	  int B = Integer.parseInt(st.nextToken());
+          System.out.println(A + B);
+          System.out.println(A - B);
+          System.out.println(A * B);
+          System.out.println(A / B);
+          System.out.println(A % B);
    }
 }
 
